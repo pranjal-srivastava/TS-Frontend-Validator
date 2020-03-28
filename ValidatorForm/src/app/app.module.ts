9e3f995bb26efe0a485bd49b5b26e622389bc5e9
pranjal-srivastava <srivastava.pranjal512@gmail.com>
2020-03-28 16:55:18 +0530

    Route Module

--- ValidatorForm/src/app/app.module.ts
+++ ValidatorForm/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+import { HttpClientModule} from "@angular/common/http";
+import { RegisterComponent } from './register/register.component';
+import {FormsModule} from "@angular/forms";
+
+
+const appRoutes : Routes = [
+
+  {path : 'register',
+    component : RegisterComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
